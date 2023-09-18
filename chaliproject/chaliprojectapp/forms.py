55351dac950107fa7ae6c31a4f8f1ad59d106b97
Charles Mbillo <82718900+CharlesMbillo@users.noreply.github.com>
2023-09-19 01:18:03 +0300

    Add files via upload

--- chaliproject/chaliprojectapp/forms.py
+++ chaliproject/chaliprojectapp/forms.py
@@ -3,8 +3,7 @@ from models import User
 from django.core.validators import EmailValidator, RegexValidator
 
     
-
-class UserForm(forms.ModelForm):
+class UserForm(forms.Form):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'phone_number', 'email']
@@ -12,3 +11,13 @@ class UserForm(forms.ModelForm):
         last_name = forms.CharField(max_length=100)
         phone_number = forms.CharField(validators=[RegexValidator(regex='^\d{15}$')])
         email = forms.EmailField(validators=[EmailValidator])
+
+        def clean_email(self):
+            email = self.cleaned_data['email']
+            if not email.endswith('@gmail.com'):
+                raise forms.ValidationError('Please enter a valid email address.')
+                
+        def clean_phone_number(self):
+            phone_number = self.cleaned_data['phone_number']
+            if not phone_number.startwith(+254):
+                raise forms.ValidationError('Please enter avalid phone number.')
